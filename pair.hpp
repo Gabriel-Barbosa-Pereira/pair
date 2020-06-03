60e9724930e69548cd3c057dab65e9411f8309cf
Gabriel Barbosa Pereira <superxl8gbpaa@outlook.com>
2020-06-03 13:18:38 -0300

    Update pair.hpp

--- pair.hpp
+++ pair.hpp
@@ -1,15 +1,22 @@
 #ifndef _PAIR_HPP_
 #define _PAIR_HPP_
 
-namespace xl{
-    template <class F, class S>
-    class Pair{
-        public:
-            Pair();
-            Pair(F one, S two);
-            F first;
-            S second;
-    };
+template <class F, class S>
+class Pair{
+    public:
+        Pair();
+        Pair(F one, S two);
+        F first;
+        S second;
 };
+
+template <class F, class S>
+Pair <F, S>::Pair(F one, S two){
+    this->first = one;
+    this->second = two;
+}
+
+template <class F, class S>
+Pair <F, S>::Pair(){}
 
 #endif
